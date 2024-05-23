c7ee6ce68e7197011ea96414305b842ae1ab6537
young-droid <young.yj.cho@gmail.com>
2024-05-23 12:03:49 +0900

    JS destructuring, scope, hoisting, etc

--- JS/destructuring.js
+++ JS/destructuring.js
@@ -8,3 +8,30 @@ const user = { id: 1, name: "Hong", addr: { city: "Seoul", road: "강남대로" 
 const { id: id } = user;
 const { id: userId, name: userName } = user;
 console.log(userId);
+
+const { name: n, age = 30 } = { name: "Lee" }; // n = ?, age = ?
+const { age2 = 30 } = { name: "Park", age2: 20 }; // age2 = 20
+const fn = ({ age }) => age; // user = {id: 1, name: 'P', age: 33}
+// ⇐⇒ function fn({age }) { return age; }
+const { age2: age3 = fn(user) } = { age22: 20 }; // age2 = 20, age3 = 33
+const u3 = { id: 3, name: "kim", addr: { id: 1, city: "Seoul" } };
+let {
+  id: idd,
+  addr: { id: aid },
+} = u3; // idd=3, aid=?
+const arr = [1, 2, [3, 4], [5, 6], { ax: 7, ay: 8 }, { ax: 9 }];
+const [, x, [, y], z] = arr; // x = ?, y = ?, z = ?
+const { 1: p, 4: q } = arr; //  p = ?, q = ?  ⇐ const [, p, , , q] = arr;
+const [, , , , { ay: a1 }, { ax: a2 }] = arr; // a1 = ?, a2 = ?
+const [k, v] = Object.entries(user);
+// const [k, v] = [ [ 'id', 1 ], [ 'name', 'P' ], [ 'age', 33 ] ]
+
+const obj = {
+  id: 1,
+  fn1: function () {
+    console.log(arguemtns);
+  },
+};
+
+const { fn1 } = obj;
+fn1(1, 2, 3);
--- JS/practice.js
+++ JS/practice.js
@@ -4,3 +4,39 @@ const dt = new Date();
 const todayName = WEEK_NAMES[dt.getDay()];
 console.log(`오늘은 ${todayName}요일 입니다.`);
 // console.log("🚀 ~ dt:", dt, dt.getDay());
+
+//user 객체를 받아서 id와 name을 출력하는 함수를 3개의 함수로 작성하시오. (function signature를 3개 이상으로 표현하기)
+
+const hong = { id: 1, name: "Hong" };
+const lee = { id: 2, name: "Lee" };
+
+// f1(hong);
+// F2;
+// 1, "Hong";
+// console.log(id, name);
+
+function f1(x, y) {}
+var f2 = function (x, y) {};
+const f3 = (x, y) => {};
+
+const f3_1 = (user) => `id=${user.id}, name=${user.name}`;
+const f3_2 = (user) => console.log(id, name);
+const f3_3 = ({ id, name }) => console.log(id, name);
+
+//다음 user 객체에서 passwd 프로퍼티를 제외한 데이터를 userInfo 라는 변수에 할당하시오.
+
+const user = { id: 1, name: "Hong", passwd: "xxx", addr: "Seoul" };
+
+//console.log(userInfo);
+// 출력결과: {id: 1, name: 'Hong', addr: 'Seoul'}
+
+//다음 arr에서 3개의 id를 id1, id2, id3로 할당하시오.
+
+const arr = [[{ id: 1 }], [{ id: 2 }, { id: 3 }]];
+const id1 = arr[0][0].id;
+const id2 = arr[1][0].id;
+const id3 = arr[1][1].id;
+console.log(id1, id2, id3);
+// 출력결과: 1 2 3
+
+console.log(Number(0.1 + 0.2).toString(2));
