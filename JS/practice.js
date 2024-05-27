6eeebaf998042e99e4574668d23389c95d96c57f
young-droid <young.yj.cho@gmail.com>
2024-05-27 17:56:52 +0900

    move JS_0527 to TIL folder

--- JS/practice.js
+++ JS/practice.js
@@ -45,56 +45,17 @@ user5 = { id: 1, name: "Hong" };
 x5 = { ...user5 };
 console.log(x5);
 
-//Rewrite the code below to use array destructuring instead of assigning each value to a variable.
-{
-  console.log("EXERCISE 1");
-
-  let item = ["Egg", 0.25, 12];
-
-  let name = item[0];
-  let price = item[1];
-  let quantity = item[2];
-
-  console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
-  console.log();
-}
-
-{
-  console.log("EXERCISE 1 practice");
-  let item = ["Egg", 0.25, 12];
-  let [name, quantity, price] = ["Egg", 0.25, 12];
-  console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
-}
-
-/**
-Exercise 2
-Rewrite the code below to assign each number to the right variable.
-*/
-{
-  console.log("EXERCISE 2");
-
-  let numbers = [3, 5, 4, 2, 6, 1];
-
-  let [one, two, three, four, five, six] = numbers;
-
-  console.log(
-    `One: ${one}, Two: ${two}, Three: ${three}, Four: ${four}, Five: ${five}, Six: ${six}`
-  );
-  console.log();
-}
-{
-  console.log("EXERCISE 2 practice");
-
-  let numbers = [3, 5, 4, 2, 6, 1];
-
-  let [three, five, four, two, six, one] = numbers;
-
-  console.log(
-    `One: ${one}, Two: ${two}, Three: ${three}, Four: ${four}, Five: ${five}, Six: ${six}`
-  );
-  console.log();
-}
-
 const fn = ({ age }) => age; // user = {id: 1, name: 'P', age: 33}
 // ⇐⇒ function fn({age }) { return age; }
 const { age2: age3 = fn(user) } = { age22: 20 }; // age2 = 20, age3 = 33
+
+function f() {
+  let count = 10;
+  return {
+    count,
+  };
+}
+
+const x = f();
+console.log("🚀 ~ x:", x);
+console.log("🚀 ~ x.count:", x.count);
