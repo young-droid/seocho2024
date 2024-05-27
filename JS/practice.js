49d2fed4164d961d3e5380b2385f2a61c38e564f
young-droid <young.yj.cho@gmail.com>
2024-05-27 10:49:08 +0900

    context scope practice

--- JS/practice.js
+++ JS/practice.js
@@ -40,3 +40,61 @@ console.log(id1, id2, id3);
 // 출력결과: 1 2 3
 
 console.log(Number(0.1 + 0.2).toString(2));
+
+user5 = { id: 1, name: "Hong" };
+x5 = { ...user5 };
+console.log(x5);
+
+//Rewrite the code below to use array destructuring instead of assigning each value to a variable.
+{
+  console.log("EXERCISE 1");
+
+  let item = ["Egg", 0.25, 12];
+
+  let name = item[0];
+  let price = item[1];
+  let quantity = item[2];
+
+  console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
+  console.log();
+}
+
+{
+  console.log("EXERCISE 1 practice");
+  let item = ["Egg", 0.25, 12];
+  let [name, quantity, price] = ["Egg", 0.25, 12];
+  console.log(`Item: ${name}, Quantity: ${quantity}, Price: ${price}`);
+}
+
+/**
+Exercise 2
+Rewrite the code below to assign each number to the right variable.
+*/
+{
+  console.log("EXERCISE 2");
+
+  let numbers = [3, 5, 4, 2, 6, 1];
+
+  let [one, two, three, four, five, six] = numbers;
+
+  console.log(
+    `One: ${one}, Two: ${two}, Three: ${three}, Four: ${four}, Five: ${five}, Six: ${six}`
+  );
+  console.log();
+}
+{
+  console.log("EXERCISE 2 practice");
+
+  let numbers = [3, 5, 4, 2, 6, 1];
+
+  let [three, five, four, two, six, one] = numbers;
+
+  console.log(
+    `One: ${one}, Two: ${two}, Three: ${three}, Four: ${four}, Five: ${five}, Six: ${six}`
+  );
+  console.log();
+}
+
+const fn = ({ age }) => age; // user = {id: 1, name: 'P', age: 33}
+// ⇐⇒ function fn({age }) { return age; }
+const { age2: age3 = fn(user) } = { age22: 20 }; // age2 = 20, age3 = 33
