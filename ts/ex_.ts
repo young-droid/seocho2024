570ced05d81c4a0f88333d3f4f7f45cea32b4033
young-droid <young.yj.cho@gmail.com>
2024-06-17 00:03:09 +0900

    typescript-constructor

--- ts/ex_.ts
+++ ts/ex_.ts
@@ -26,23 +26,26 @@ console.log('🚀 ~ counts.apple:', counts.apple);
 
 console.log('============================');
 
-// type Ud2 = (TUser | TDept) & { addr: string };
+type Ud2 = (User | Dept) & { addr: string };
 
-interface User {
-  id: number;
-  name: string;
-}
+// interface User {
+//   id: number;
+//   name: string;
+// }
 
-interface Dept {
-  id: number;
-  dname: string;
-  captain: string;
-}
-interface Ud2 {
-  // <이 부분을 작성하시오>
-  addr: string;
-}
+// interface Dept {
+//   id: number;
+//   dname: string;
+//   captain: string;
+// }
+// interface Ud2 {
+//   [key: string]: string | number
+//   id: number;
+//   addr: string;
+// }
 
 // 다음 코드가 오류가 없으면 통과!
 const ud2: Ud2 = { id: 1, name: 'HH', addr: 'Seoul' };
 const ud3: Ud2 = { id: 1, dname: 'HH', captain: 'HH', addr: 'Seoul' };
+
+ud2.
